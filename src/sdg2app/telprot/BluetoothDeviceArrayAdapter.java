41ce237a71faf0fe71ed335d4f89bbdb91991eb5
ballesterosbr <ballesterosbr@users.noreply.github.com>
2015-07-06 19:21:32 +0200

    Update BluetoothDeviceArrayAdapter.java

--- src/sdg2app/telprot/BluetoothDeviceArrayAdapter.java
+++ src/sdg2app/telprot/BluetoothDeviceArrayAdapter.java
@@ -1,7 +1,6 @@
 package sdg2app.telprot;
 
 import java.util.List;
-
 import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -11,17 +10,19 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 public class BluetoothDeviceArrayAdapter extends ArrayAdapter{
-	
+
 	private List<BluetoothDevice> deviceList;
 	private Context context;
-	
+
+	/////////////////////////////////////////----ADAPTADOR PARA EL LISTVIEW DISPOSITIVOS---////////////////////////////////////////////
 	public BluetoothDeviceArrayAdapter(Context context, int textViewResourceId, List<BluetoothDevice> objects){
 		super(context, textViewResourceId, objects);
-		
+
 		this.deviceList = objects;
 		this.context = context;
 	}
-	
+
+	// Contador para saber los elementos y así las posiciones.
 	@Override
 	public int getCount(){
 		if(deviceList != null){
@@ -30,43 +31,41 @@ public class BluetoothDeviceArrayAdapter extends ArrayAdapter{
 			return 0;
 		}
 	}
-	
+
+	// Selecciona item a partir de la posición
 	@Override
 	public Object getItem(int position){
 		return(deviceList == null ? null : deviceList.get(position));
 	}
-	
+
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent){
 		if((deviceList == null) || (context == null))
 			return null;
 
-		// Usamos un LayoutInflater para crear las vistas
+		// Con un Layout inflater creamos la vista
 		LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		
-		// Creamos una vista a partir de simple_list_item_2, que contiene dos TextView.
-		// El primero (text1) lo usaremos para el nombre, mientras que el segundo (text2)
-		// lo utilizaremos para la direccion del dispositivo.
-		View elemento = inflater.inflate(android.R.layout.simple_list_item_2, parent, false);
-		
-		// Referenciamos los TextView
-		TextView txtNombre = (TextView)elemento.findViewById(android.R.id.text1);
-		TextView txtDireccion = (TextView)elemento.findViewById(android.R.id.text2);
-		
-		// Obtenemos el dispositivo del array y obtenemos su nombre y direccion, asociandosela
-		// a los dos TextView del elemento
+
+		// Usamos simple_list_item2 porque permite dos renglones (dos textview).
+		// Text1 será para el nombre del dispositivo
+		// Text2 será para la dirección física del dispositivo
+		View vista = inflater.inflate(android.R.layout.simple_list_item_2, parent, false);
+		TextView nombreDispositivo = (TextView)vista.findViewById(android.R.id.text1);
+		TextView direccionDispositivo = (TextView)vista.findViewById(android.R.id.text2);
+
+		// Una vez tenemos el dispositivo a partir de su posición, le asociamos nombre y direccion
 		BluetoothDevice dispositivo = (BluetoothDevice)getItem(position);
 		if(dispositivo != null)
 		{
-			txtNombre.setText(dispositivo.getName());
-			txtDireccion.setText(dispositivo.getAddress());
+			nombreDispositivo.setText(dispositivo.getName());
+			direccionDispositivo.setText(dispositivo.getAddress());
 		}
 		else
 		{
-			txtNombre.setText("ERROR");
+			nombreDispositivo.setText("ERROR");
 		}
-		
-		// Devolvemos el elemento con los dos TextView cumplimentados
-		return elemento;
+
+		// return de los dos Textview
+		return vista;
 	}
 }
